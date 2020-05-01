9ae264b491e6b01d5b62a43424e5be5ae093d0cd
imvicky97 <vsai97+github@gmail.com>
2020-05-01 21:32:14 +0530

    Commit #1 - Get all restaurants

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/RestaurantDao.java
@@ -0,0 +1,34 @@
+package com.upgrad.FoodOrderingApp.service.dao;
+
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantCategoryEntity;
+import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class RestaurantDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public TypedQuery<RestaurantEntity> getAllRestaurants() {
+        try {
+            return entityManager.createNamedQuery("getAllRestaurants", RestaurantEntity.class);
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+    public List<RestaurantCategoryEntity> getCategoryByRestaurant(RestaurantEntity restaurantEntity) {
+        try {
+            return entityManager.createNamedQuery("getCategoryByRestaurant", RestaurantCategoryEntity.class).setParameter("restaurantId", restaurantEntity).getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+
+}
